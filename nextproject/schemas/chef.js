031ac6e602111c989ee811e3d4658f31d047cf7e
andreyscott <andreyscott301@gmail.com>
2022-06-13 20:27:36 +0100

    made you

---
+++ nextproject/schemas/chef.js
@@ -0,0 +1,21 @@
+export default {
+    name: 'chef',
+    title: 'Chef',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: "Chef's Title",
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image'
+
+}
